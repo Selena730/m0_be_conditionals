23c09aedcc00a873c46ac1a55e1f276df684770a
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-11-11 12:19:47 -0700

    Solve spicy challenge

---
+++ code_challenges/spicy_code_challenge.rb
@@ -0,0 +1,15 @@
+# This is The Spicy Code Challenge
+
+
+def Fizz_Buzz(number)
+
+if  number % 3 == 0 && number % 5 == 0
+    print "FizzBuzz"
+ elsif number % 5 == 0
+    print "Buzz"
+ elsif number % 3 == 0
+    print "Fizz" 
+ end       
+end
+
+Fizz_Buzz(12) 
